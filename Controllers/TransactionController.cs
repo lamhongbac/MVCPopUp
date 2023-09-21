b27b3d333a13ccc57f3b609fe95ae36f1e0ee176
lamhong.bac <lamhong.bac@gmail.com>
2023-09-21 17:13:49 +0700

    Add project files.

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCPopUp.Models;
+
+namespace MVCPopUp.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+        public TransactionController(TransactionDbContext context)
+        {
+                _context=context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new TransactionModel());
+            else
+            {
+                var transactionModel = await _context.Transactions.FindAsync(id);
+                if (transactionModel == null)
+                {
+                    return NotFound();
+                }
+                return View(transactionModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //Insert
+                if (id == 0)
+                {
+                    transactionModel.Date = DateTime.Now;
+                    _context.Add(transactionModel);
+                    await _context.SaveChangesAsync();
+
+                }
+                //Update
+                else
+                {
+                    try
+                    {
+                        _context.Update(transactionModel);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!TransactionModelExists(transactionModel.TransactionId))
+                        { return NotFound(); }
+                        else
+                        { throw; }
+                    }
+                }
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Transactions.ToList()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", transactionModel) });
+        }
+        private bool TransactionModelExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionId == id);
+        }
+    }
+}
